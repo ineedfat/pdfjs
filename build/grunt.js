801d200ac23d92fdda4b156192915386ecb1bb7c
Trinh <tho@cs.washington.edu>
2013-01-22 13:31:21 -0800

    Adding grunt build files

---
+++ build/grunt.js
@@ -0,0 +1,66 @@
+﻿/*global module:false*/
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: '<json:package.json>',
+        meta: {
+            banner: '/*! <%= pkg.title %> | <%= pkg.homepage %> | ' +
+              '<%= grunt.template.today("yyyy-mm-dd") %> | <%= pkg.author.name %>(<%= pkg.author.url %>)*/\nvar PDFJS_VERSION = <%= pkg.version %>'
+        },
+        lint: {
+            files: [
+              '../src/core.js',
+              '../src/catalog.js',
+              '../src/font.js',
+              '../src/info.js',
+              '../src/page.js',
+              '../src/resources.js',
+              '../src/trailer.js',
+              '../src/utils.js'
+            ]
+        },
+        concat: {
+            dist: {
+                src: ['<banner>', '<config:lint.files>'],
+                dest: '../<%= pkg.name %>.js'
+            },
+        },
+        min: {
+            dist: {
+                src: ['<banner>', '<config:lint.files>'],
+                dest: '../<%= pkg.name %>.min.js'
+            },
+        },
+        watch: {
+            cj: {
+                files: '<config:lint.files>',
+                tasks: 'concat min'
+            }
+        },
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true,
+                browser: true
+            },
+            globals: {
+                jQuery: true,
+                pdfJS: true
+            }
+        },
+        uglify: {}
+    });
+
+    // by default only concat cj files
+    grunt.registerTask('default', 'concat min');
+
+};
